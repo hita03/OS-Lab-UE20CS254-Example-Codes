22e90d69105815647186201f9b70584c4061d286
Hita Juneja <hitajuneja@gmail.com>
2022-03-10 17:13:49 +0530

    Update prgm5.c

--- Week 7 - Unit 2/prgm5.c
+++ Week 7 - Unit 2/prgm5.c
@@ -1,4 +1,4 @@
-//working with 2 mutex locks for threads
+//demonstrates deadlock 
 pthread_t tid1,tid2;
 pthread_mutex_t first_mutex,second_mutex;
 
@@ -7,22 +7,22 @@ void *do_work_one(void *param)
 {
 pthread_mutex_lock(&first_mutex);
 pthread_mutex_lock(&second_mutex);
-printf("Enterin critical session\n");
+printf("Entering critical session -tid1\n");
 sleep(5);
-printf("exiting critical sesssion\n");
+printf("exiting critical sesssion -tid1\n");
 pthread_mutex_unlock(&first_mutex);
 pthread_mutex_unlock(&second_mutex);
 pthread_exit(0);
 }
 
-
+//thread two runs in this function
 void *do_work_two(void *param)
 {
 pthread_mutex_lock(&first_mutex);
 pthread_mutex_lock(&second_mutex);
-printf("Enterin critical session\n");
+printf("Entering critical session - tid2\n");
 sleep(5);
-printf("exiting critical sesssion\n");
+printf("exiting critical sesssion - tid2\n");
 pthread_mutex_unlock(&second_mutex);
 pthread_mutex_unlock(&first_mutex);
 pthread_exit(0);
